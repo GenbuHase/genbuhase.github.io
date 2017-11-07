1d9521c4b73154383a26630cd3b29a62840084ec
Genbu Hase <hasegenbu@gmail.com>
2017-11-07 19:12:15 +0900

    コミット

--- Top/Top.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener("DOMContentLoaded", () => {
-	setTimeout(() => {
-		gapi.person.go();
-	}, 500);
-});
--- main.js
+++ main.js
@@ -3,4 +3,8 @@ window.addEventListener("DOMContentLoaded", () => {
 		let drawer = new mdc.drawer.MDCPersistentDrawer(DOM("#Drawer"));
 			drawer.open = !drawer.open;
 	});
+
+	DOM("#Content_Page").addEventListener("load", (event) => {
+		new mdc.drawer.MDCPersistentDrawer(DOM("#Drawer")).open = false;
+	});
 });
