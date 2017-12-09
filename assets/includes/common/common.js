dc341fe75df978d4d083cbe5c373ef6da0578b4c
Genbu Hase <hasegenbu@gmail.com>
2017-12-09 18:39:25 +0900

    いい感じー

--- assets/includes/common/common.js
+++ assets/includes/common/common.js
@@ -2,23 +2,14 @@ window.addEventListener("DOMContentLoaded", () => {
 	let menuContainer = new DOM.ComponentLoader("/assets/includes/common/").doc.querySelector("Body");
 		menuContainer.childNodes.forEach(component => document.body.appendChild(component));
 
-		new DOM('$*[Data-Component="Content"]').appendChild(new DOM("$Main"));
+		new DOM('$*[Data-Component="Frame-Content"]').appendChild(new DOM("$Main"));
 
-		new DOM('$*[Data-Component="Content_Toolbar_DrawerBtn"]').addEventListener("click", () => {
-			let drawer = new mdc.drawer.MDCPersistentDrawer(new DOM('$*[Data-Component="Drawer"]'));
+		new DOM('$*[Data-Component="Frame-Content_Toolbar_DrawerBtn"]').addEventListener("click", () => {
+			let drawer = new mdc.drawer.MDCPersistentDrawer(new DOM('$*[Data-Component="Frame-Drawer"]'));
 				drawer.open = !drawer.open;
 		});
 		
 	window.mdc.autoInit();
-
-	new DOM('@A[Href]:Not([Href=""]):Not([Href="#"]):Not([Href^="javascript:"])').forEach(anchor => {
-		anchor.addEventListener("click", (event) => {
-			if (parent.document.querySelector("#Content_Page")) {
-				event.preventDefault();
-				parent.document.querySelector("#Content_Page").src = anchor.href;
-			}
-		});
-	});
 
 	new DOM("@Main").forEach(elem => {
 		elem.classList.add("mdc-typography");
