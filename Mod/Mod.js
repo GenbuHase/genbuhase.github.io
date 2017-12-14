26dbb8f71b4c812e83420ad8382a292aed298fc3
Genbu Hase <hasegenbu@gmail.com>
2017-12-14 20:34:28 +0900

    とりま全Modを羅列。

--- Mod/Mod.js
+++ Mod/Mod.js
@@ -22,7 +22,23 @@ class Mod {
 
 let modInfo = [
 	{ name: "Advanced Weapon", url: "Advanced Weapon" },
-	{ name: "More Records", url: "More Records" }
+	{ name: "Animal Rider", url: "Animal Rider", noImage: true },
+	{ name: "Command Tools", url: "Command Tools", noImage: true },
+	{ name: "Customizable Blocks", url: "Customizable Blocks", noImage: true },
+	{ name: "Data Helper", url: "API/Data Helper", noImage: true },
+	{ name: "Earth Defence Force For MCPE", url: "Earth Defence Force For MCPE", noImage: true },
+	{ name: "GUI Helper", url: "API/GUI Helper", noImage: true },
+	{ name: "Many Phones", url: "Many Phones", noImage: true },
+	{ name: "More Records", url: "More Records" },
+	{ name: "Reality Building", url: "Reality Building", noImage: true },
+	{ name: "Scripting+", url: "API/Scripting+", noImage: true },
+	{ name: "Too Many Bombs", url: "Too Many Bombs", noImage: true },
+	{ name: "Trap Tools", url: "Trap Tools", noImage: true },
+	{ name: "Vehicle Rider", url: "Vehicle Rider", noImage: true },
+	{ name: "Villager's Inventory", url: "Villager's Inventory", noImage: true },
+	{ name: "WorldEdit", url: "WorldEdit", noImage: true },
+	{ name: "Zero Gravity", url: "Zero Gravity", noImage: true },
+	{ name: "まさにカオス", url: "まさにカオス", noImage: true }
 ];	modInfo.forEach(mod => {
 	new Mod(mod.name, mod.url, mod.noImage).append();
 });
