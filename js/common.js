7f1749cce96fecd13d7c22e4821d03fb5d8c2511
Genbu Hase <hasegenbu@gmail.com>
2018-12-02 13:59:05 +0900

    どろういんぐ。実装

--- js/common.js
+++ js/common.js
@@ -74,4 +74,10 @@ h={};f()}},R=Q.define;"function"===typeof R&&R.amd&&R("google-code-prettify",[],
 window.addEventListener("DOMContentLoaded", () => {
 	const sidenavs = document.querySelectorAll(".sidenav");
 	for (const sidenav of sidenavs) M.Sidenav.init(sidenav);
+
+	const carousels = document.querySelectorAll(".carousel");
+	for (const carousel of carousels) M.Carousel.init(carousel);
+
+	const materialboxes = document.querySelectorAll(".materialboxed");
+	for (const materialbox of materialboxes) M.Materialbox.init(materialbox);
 });
--- src/!libs/common.js
+++ src/!libs/common.js
@@ -3,4 +3,10 @@
 window.addEventListener("DOMContentLoaded", () => {
 	const sidenavs = document.querySelectorAll(".sidenav");
 	for (const sidenav of sidenavs) M.Sidenav.init(sidenav);
+
+	const carousels = document.querySelectorAll(".carousel");
+	for (const carousel of carousels) M.Carousel.init(carousel);
+
+	const materialboxes = document.querySelectorAll(".materialboxed");
+	for (const materialbox of materialboxes) M.Materialbox.init(materialbox);
 });
