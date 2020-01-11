c26f089050f139a9a53f4c98c0f591a5a29d36f4
Genbu Hase <hasegenbu@gmail.com>
2020-01-11 15:18:34 +0900

    数種の変更 * /以外の経由アクセスでも表示されるように * モバイル表示でのページ遷移時にSideNavを閉じるように   * $navbar-breakpointを追加

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,35 @@
 <template>
 	<Div ID = "App">
 		<Navigation />
-		<router-view />
+
+		<transition name = "pageRouting" mode = "out-in" @enter = "onRoutingEnter">
+			<router-view />
+		</transition>
 	</Div>
 </template>
 
 <style lang="scss">
 	@import "./public/libs/Materialize-v1.0.0/sass/materialize";
+</style>
+
+<style lang="scss" scoped>
+	.pageRouting {
+		&-enter-active, &-leave-active {
+			transition: opacity 0.25s;
+		}
+
+		&-enter, &-leave-to {
+			opacity: 0;
+		}
+	}
+</style>
+
+<style lang="scss" scoped>
+	@import "./public/libs/Materialize-v1.0.0/sass/components/color-variables";
+	@import "./public/libs/Materialize-v1.0.0/sass/components/variables";
+	@import "./public/libs/Materialize-v1.0.0/sass/components/extends/extends-variables";
+	
+	:root { -breakpoint: $navbar-breakpoint-width }
 </style>
 
 <script>
@@ -16,8 +39,29 @@
 	export default {
 		components: { Navigation },
 
+		computed: {
+			breakpoint () {
+				for (const tag of Array.from(document.getElementsByTagName("style"))) {
+					const matched = tag.innerText.match(/\-breakpoint: (\d+)px;/);
+					
+					if (matched) return parseInt(matched[1], 10);
+				}
+
+				return 0;
+			}
+		},
+
 		mounted () {
 			M.AutoInit();
+		},
+
+		methods: {
+			onRoutingEnter () {
+				if (this.breakpoint < window.innerWidth) return;
+
+				const sideNav = this.$el.querySelector("#navigation_sideNav");
+				(sideNav && sideNav.M_Sidenav && sideNav.M_Sidenav.isOpen) && (sideNav.M_Sidenav.close());
+			}
 		}
 	};
 </script>
