042ed28c87d42b63192b7992d48c7517cddb0611
Genbu Hase <14960841+GenbuHase@users.noreply.github.com>
2021-08-22 14:47:48 +0900

    2.0.21 - 旧構成を削除

--- !OLD/src/Mod/Contents/まさにカオス/download/Release/まさにカオス Release 1.00.js
+++
@@ -1,6 +0,0 @@
-var Badass;
-
-function deathHook(murderer,victim){
-	Badass = Level.spawnMob(Entity.getX(victim), Entity.getY(victim), Entity.getZ(victim), Entity.getEntityTypeId(victim));
-	Entity.addEffect(Badass, MobEffect.damageBoost && MobEffect.movementSpeed && MobEffect.regeneration, 1000000 * 20, 16, false, true);
-}
